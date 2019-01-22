223cc36b203a56b4fdbaed4033eabe771f5514b0
Miguel Ángel Díaz <miguelangel@MacBook-Pro-de-Miguel.local>
2019-01-22 23:55:08 +0100

    added dependencies to formula
    update nef version - 0.1.1
    
    added support to NEF
    
    minor changes
    
    minor changes
    
    update formula to 0.1.1 release version

--- nef.rb
+++ nef.rb
@@ -1,10 +1,14 @@
 class Nef < Formula
   desc "A command line tool to compile Swift documentation written in Playground format with Bow support"
   homepage ""
-  url "https://github.com/bow-swift/nef/archive/0.1.0.tar.gz"
-  sha256 "22108b6f2845e22581af216f87b93340884dbd5d66ef0ab0e38e15ee1a9cf38d"
+  url "https://github.com/bow-swift/nef/archive/0.1.1.tar.gz"
+  sha256 "88e285bbf0d8f977837bca2c6f6587f0a68905285e08dcb8e2c6c9e37fd39d89"
+
+  depends_on "cocoapods"
+  depends_on :xcode => "10.0"
 
   def install
+    bin.install "./bin/nef"
     bin.install "./bin/nefc"
     bin.install "./bin/nef-playground"
   end
