13c5a74c38051b8bf6b39835f29abce21a3b944d
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-01-23 15:26:35 +0100

    Prerelease 0.1.0 - nef early access

--- nef.rb
+++ nef.rb
@@ -1,8 +1,8 @@
 class Nef < Formula
   desc "A command line tool to compile Swift documentation written in Playground format with Bow support"
   homepage ""
-  url "https://github.com/bow-swift/nef/archive/0.1.1.tar.gz"
-  sha256 "88e285bbf0d8f977837bca2c6f6587f0a68905285e08dcb8e2c6c9e37fd39d89"
+  url "https://github.com/bow-swift/nef/archive/0.1.0.tar.gz"
+  sha256 "03eaf838606c182fe5a200f71b1aa4eb8f98b90eb38b115087d27a2a2db953f8"
 
   depends_on "cocoapods"
   depends_on :xcode => "10.0"
