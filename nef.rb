305fb081e29199e6f9b1dcaf58bbde7a249d4c1e
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-02-27 16:40:09 +0100

    update to 0.1.2
    update nef project to 0.1.2

--- nef.rb
+++ nef.rb
@@ -1,8 +1,8 @@
 class Nef < Formula
   desc "A command line tool to compile Swift documentation written in Playground format with Bow support"
   homepage ""
-  url "https://github.com/bow-swift/nef/archive/0.1.1.tar.gz"
-  sha256 "4b1edb73280512d8665b401c6b26d7b7fe370948df13dedf8b31b0759532fbb0"
+  url "https://github.com/bow-swift/nef/archive/0.1.2.tar.gz"
+  sha256 "77e6faf6cba6e664451766f21415ae9312d4929a2ef21c4e496ca287c9b0ce2b"
 
   depends_on "cocoapods"
   depends_on :xcode => "10.0"
