b7f090526f9fb1480a50aa6421ad6f150321351c
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-03-01 17:40:44 +0100

    install `nef-jekyll` in `nef`
    release pre-stable nef
    
    pre-release 0.1.5
    
    pre-release

--- nef.rb
+++ nef.rb
@@ -1,8 +1,8 @@
 class Nef < Formula
   desc "A command line tool to compile Swift documentation written in Playground format with Bow support"
   homepage ""
-  url "https://github.com/bow-swift/nef/archive/0.1.2.tar.gz"
-  sha256 "77e6faf6cba6e664451766f21415ae9312d4929a2ef21c4e496ca287c9b0ce2b"
+  url "https://github.com/bow-swift/nef/archive/0.1.6.tar.gz"
+  sha256 "2e0e6dace7d13d53a3180619d7ed6db58717aa81dd1875c1c1c83986d80939ae"
 
   depends_on "cocoapods"
   depends_on :xcode => "10.0"
@@ -14,6 +14,7 @@ class Nef < Formula
     bin.install "./bin/nefc"
     bin.install "./bin/nef-playground"
     bin.install "./bin/nef-jekyll-page"
+    bin.install "./bin/nef-jekyll"
   end
 
   def buildJekyllPage
