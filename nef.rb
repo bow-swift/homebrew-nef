db0b10859ca8aa555dac21a38871df9ea2bbbb67
Tomás Ruiz-López <truizlop@gmail.com>
2019-03-19 17:49:49 +0100

    Fix audit errors

--- nef.rb
+++ nef.rb
@@ -1,5 +1,5 @@
 class Nef < Formula
-  desc "A command line tool to compile Swift documentation written in Playground format with Bow support"
+  desc "Tool to compile Swift docs written in Playground format with Bow support"
   homepage "https://github.com/bow-swift/nef"
   url "https://github.com/bow-swift/nef/archive/0.1.0.tar.gz"
   sha256 "ec982a271c55e0003f03821dbf8f11306b28f232c8a6df3f5cbfdb11b8d4cbf5"
@@ -8,8 +8,7 @@ class Nef < Formula
   depends_on :xcode => "10.0"
 
   def install
-    buildJekyllPage
-
+    build_jekyll_page
     bin.install "./bin/nef"
     bin.install "./bin/nefc"
     bin.install "./bin/nef-playground"
@@ -17,10 +16,14 @@ class Nef < Formula
     bin.install "./bin/nef-jekyll"
   end
 
-  def buildJekyllPage
-    system "xcodebuild", "-project", "./markdown/JekyllMarkdown.xcodeproj",  "-scheme", "JekyllMarkdown", "clean", "build", "-derivedDataPath", "/tmp/nef"
-    system "mv", "/tmp/nef/Build/Products/Release/JekyllMarkdown", "./bin/nef-jekyll-page"
-    system "rm", "-rf", "/tmp/nef/"
+  test do
+    print ""
   end
 
+  def build_jekyll_page
+    xcodebuild "-project", "./markdown/JekyllMarkdown.xcodeproj", "-scheme", "JekyllMarkdown", "-configuration", "Release", "clean", "build",
+    "SYMROOT=/tmp/nef/Build/Products", "-derivedDataPath", "/tmp/nef"
+    mv "/tmp/nef/Build/Products/Release/JekyllMarkdown", "./bin/nef-jekyll-page"
+    rm "/tmp/nef/", :force => true
+  end
 end
