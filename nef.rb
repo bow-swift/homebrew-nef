303b9c712c30e1710f024cced54a7c6a305ee9d7
Miguel Ángel <madiaz.dev@gmail.com>
2019-04-18 10:47:27 +0200

    Update nef.rb
    Swift 5

--- nef.rb
+++ nef.rb
@@ -1,10 +1,10 @@
 class Nef < Formula
   desc "Tool to compile Swift docs written in Playground format with Bow support"
   homepage "https://github.com/bow-swift/nef"
-  url "https://github.com/bow-swift/nef/archive/0.1.4.tar.gz"
-  sha256 "6a85f474b6c635eb88f62666eafbf1104b87ed370fc3e45d6b28ab3f26e73b2d"
+  url "https://github.com/bow-swift/nef/archive/0.1.5.tar.gz"
+  sha256 "72f95490a4361ae09b85f956ace0e5b5350ac5bd08fc6a6ab0e3482a1c6cae1c"
 
-  depends_on :xcode => "10.0"
+  depends_on :xcode => "10.2"
 
   def install
     build_jekyll_page
