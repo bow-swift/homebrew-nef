251d6dfd87cb7ab05f0ae164a6473d7e0db0d62b
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-05-28 11:34:27 +0200

    Update nef.rb

--- nef.rb
+++ nef.rb
@@ -1,28 +1,42 @@
 class Nef < Formula
   desc "Tool to compile Swift docs written in Playground format with Bow support"
   homepage "https://github.com/bow-swift/nef"
-  url "https://github.com/bow-swift/nef/archive/0.1.7.tar.gz"
-  sha256 "4401274c3c889fac59fa706c6e19204eea277d48545d1b2c70da476f89fad50f"
+  url "https://github.com/bow-swift/nef/archive/0.2.0.tar.gz"
+  sha256 "4d45f2653504c91d621c922ea27b4feec7358cb462474f918eba07648108f17c"
 
   depends_on :xcode => "10.2"
 
   def install
-    build_jekyll_page
+    build_project
+
     bin.install "./bin/nef"
+    bin.install "./bin/nef-common"
     bin.install "./bin/nefc"
     bin.install "./bin/nef-playground"
     bin.install "./bin/nef-jekyll-page"
     bin.install "./bin/nef-jekyll"
+    bin.install "./bin/nef-markdown-page"
+    bin.install "./bin/nef-markdown"
   end
 
   test do
     print ""
   end
 
-  def build_jekyll_page
-    xcodebuild "-project", "./markdown/Markdown.xcodeproj", "-scheme", "JekyllMarkdown", "-configuration", "Release", "clean", "build",
-    "SYMROOT=/tmp/nef/Build/Products", "-derivedDataPath", "/tmp/nef"
-    mv "/tmp/nef/Build/Products/Release/JekyllMarkdown", "./bin/nef-jekyll-page"
-    rm "/tmp/nef/", :force => true
+  def build_project
+    build_jekyll_project
+    build_mardown_project
+  end
+
+  def build_jekyll_project
+      xcodebuild "-project", "./markdown/Markdown.xcodeproj", "-scheme", "JekyllMarkdown", "-configuration", "Release", "clean", "build", "SYMROOT=/tmp/nef/Build/Products", "-derivedDataPath", "/tmp/nef"
+      mv "/tmp/nef/Build/Products/Release/JekyllMarkdown", "./bin/nef-jekyll-page"
+      rm "/tmp/nef/", :force => true
+  end
+
+  def build_mardown_project
+      xcodebuild "-project", "./markdown/Markdown.xcodeproj", "-scheme", "Markdown", "-configuration", "Release", "clean", "build", "SYMROOT=/tmp/nef/Build/Products", "-derivedDataPath", "/tmp/nef"
+      mv "/tmp/nef/Build/Products/Release/Markdown", "./bin/nef-markdown-page"
+      rm "/tmp/nef/", :force => true
   end
 end
