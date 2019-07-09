ad0a71465613d328f36e4975e545a099957d5059
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-07-09 18:32:58 +0200

    Update nef.rb

--- nef.rb
+++ nef.rb
@@ -1,8 +1,8 @@
 class Nef < Formula
   desc "Tool to compile Swift docs written in Playground format with Bow support"
   homepage "https://github.com/bow-swift/nef"
-  url "https://github.com/bow-swift/nef/archive/0.2.2.tar.gz"
-  sha256 "1a44d2c5026b8ab591dc50045001460efa9ead1f510ad30e2ccc69222df468e7"
+  url "https://github.com/bow-swift/nef/archive/0.3.0.tar.gz"
+  sha256 "0556227f4ebe81cdfaaa6be394dcad4e5003568144da45d008aded144648fe09"
 
   depends_on :xcode => "10.2"
 
@@ -13,10 +13,12 @@ class Nef < Formula
     bin.install "./bin/nef-common"
     bin.install "./bin/nefc"
     bin.install "./bin/nef-playground"
-    bin.install "./bin/nef-jekyll-page"
-    bin.install "./bin/nef-jekyll"
     bin.install "./bin/nef-markdown-page"
     bin.install "./bin/nef-markdown"
+    bin.install "./bin/nef-jekyll-page"
+    bin.install "./bin/nef-jekyll"
+    bin.install "./bin/nef-carbon-page"
+    bin.install "./bin/nef-carbon"
   end
 
   test do
@@ -24,8 +26,15 @@ class Nef < Formula
   end
 
   def build_project
-    build_jekyll_project
     build_mardown_project
+    build_jekyll_project
+    build_carbon_project
+  end
+
+  def build_mardown_project
+      xcodebuild "-project", "./markdown/Markdown.xcodeproj", "-scheme", "Markdown", "-configuration", "Release", "clean", "build", "SYMROOT=/tmp/nef/Build/Products", "-derivedDataPath", "/tmp/nef"
+      mv "/tmp/nef/Build/Products/Release/Markdown", "./bin/nef-markdown-page"
+      rm "/tmp/nef/", :force => true
   end
 
   def build_jekyll_project
@@ -34,9 +43,9 @@ class Nef < Formula
       rm "/tmp/nef/", :force => true
   end
 
-  def build_mardown_project
-      xcodebuild "-project", "./markdown/Markdown.xcodeproj", "-scheme", "Markdown", "-configuration", "Release", "clean", "build", "SYMROOT=/tmp/nef/Build/Products", "-derivedDataPath", "/tmp/nef"
-      mv "/tmp/nef/Build/Products/Release/Markdown", "./bin/nef-markdown-page"
+  def build_carbon_project
+      xcodebuild "-project", "./markdown/Markdown.xcodeproj", "-scheme", "Carbon", "-configuration", "Release", "clean", "build", "SYMROOT=/tmp/nef/Build/Products", "-derivedDataPath", "/tmp/nef"
+      mv "/tmp/nef/Build/Products/Release/Carbon", "./bin/nef-carbon-page"
       rm "/tmp/nef/", :force => true
   end
 end
