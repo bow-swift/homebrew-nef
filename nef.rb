c248a3244367943d4257cd2af8f3a131850ac214
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-05-05 09:11:06 +0200

    Update nef.rb

--- nef.rb
+++ nef.rb
@@ -5,7 +5,7 @@ class Nef < Formula
   sha256 "182d5f91e3048f046a63bf81d148f94acc7c667b5314cf76f36a9fb75efdb3a7"
 
   depends_on :xcode => "11.4"
-  
+
   def install
     system "make", "install", "prefix=#{prefix}", "version=#{version}"
   end
@@ -14,6 +14,6 @@ class Nef < Formula
     system "#{bin}/nef", "markdown",
            "--project", "#{share}/tests/Documentation.app",
            "--output", "#{testpath}/nef"
-    assert_path_exist "#{testpath}/nef/resources.md", :exist?
+    assert_path_exist "#{testpath}/nef/library/apis.md", :exist?
   end
 end
